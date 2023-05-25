896e80f1bad8f9465177ce93baecb21e4879f37e
NaveenJsr <naveenjsr99@gmail.com>
2023-05-25 09:30:53 +0530

    Exercise 4.7: helper functions and unit tests, step5 Complete

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -41,7 +41,109 @@ const mostBlogs = ( blogs ) =>
     }, {} );
 
     let most = {
-        author: "",
+        author: "", const dummy = ( blogs ) =>
+        {
+            return 1;
+        };
+
+        const totalLikes = ( blogs ) =>
+        {
+            return blogs.reduce( ( acc, b ) =>
+            {
+                return ( acc += b.likes );
+            }, 0 );
+        };
+
+        const favoriteBlog = ( blogs ) =>
+        {
+            let result = blogs[ 0 ];
+
+            blogs.forEach( ( b ) =>
+            {
+                if ( b.likes > result.likes )
+                {
+                    result = { ...b };
+                }
+            } );
+            return result;
+        };
+
+        const mostBlogs = ( blogs ) =>
+        {
+            let res = blogs.reduce( ( acc, b ) =>
+            {
+                let key = b.author;
+                if ( acc.hasOwnProperty( key ) )
+                {
+                    acc[ key ] = ++acc[ key ];
+                } else
+                {
+                    acc[ key ] = 1;
+                }
+                return acc
+            }, {} );
+
+            let most = {
+                author: "",
+                blogs: 0
+            }
+
+            for ( author in res )
+            {
+                if ( res[ author ] > most.blogs )
+                {
+                    most.author = author
+                    most.blogs = res[ author ]
+                }
+            }
+
+            return most;
+        };
+
+
+        const mostLikes = ( blogs ) =>
+        {
+            let allAuthAndLike = blogs.reduce( ( acc, curr ) =>
+            {
+
+                if ( acc[ curr.author ] )
+                {
+                    acc[ curr.author ] += curr.likes
+                }
+                else
+                {
+                    acc[ curr.author ] = curr.likes
+                }
+
+                return acc
+            }, {} )
+
+
+            let res = {
+                author: "",
+                likes: 0
+            }
+
+            for ( author in allAuthAndLike )
+            {
+                if ( allAuthAndLike[ author ] > res.likes )
+                {
+                    res.author = author
+                    res.likes = allAuthAndLike[ author ]
+                }
+            }
+
+            return res
+        }
+
+module.exports = {
+            dummy,
+            totalLikes,
+            favoriteBlog,
+            mostBlogs,
+            mostLikes
+        };
+
         blogs: 0
     }
 
