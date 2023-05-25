68dd9a800a7db4ee64f39a399a13792ee382a733
NaveenJsr <naveenjsr99@gmail.com>
2023-05-25 10:04:52 +0530

    Exercise 4.13 Blog list expansions, step1 Complete

--- blog/tests/blog_api.test.js
+++ blog/tests/blog_api.test.js
@@ -12,65 +12,103 @@ beforeEach( async () =>
 
 } )
 
-test( 'empty notes', async () =>
+describe( 'basicDBTest', () =>
 {
-    let res = await api
-        .get( '/api/blogs' )
-        .expect( 200 )
-        .expect( 'Content-Type', /application\/json/ )
-    expect( res.body ).toHaveLength( helper.initialBlog.length )
-} )
+    test( 'empty notes', async () =>
+    {
+        let res = await api
+            .get( '/api/blogs' )
+            .expect( 200 )
+            .expect( 'Content-Type', /application\/json/ )
+        expect( res.body ).toHaveLength( helper.initialBlog.length )
+    } )
 
-test( 'check id', async () =>
+    test( 'check id', async () =>
+    {
+        let res = await api.get( '/api/blogs' )
+        // console.log(res.body)
+        expect( res.body[ 0 ].id ).toBeDefined()
+    } )
+
+} );
+
+describe( 'testing POST req', () =>
 {
-    let res = await api.get( '/api/blogs' )
-    // console.log(res.body)
-    expect( res.body[ 0 ].id ).toBeDefined()
-} )
+    test( 'checking add blog', async () =>
+    {
+        let payload = {
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+        }
 
-test( 'checking add blog', async () =>
+        await api.post( "/api/blogs" ).send( payload ).expect( 201 )
+
+        let allBlog = await helper.blogInDB()
+        expect( allBlog.length === helper.initialBlog.length + 1 )
+    } )
+
+    test( "likes default 0", async () =>
+    {
+        let payload = {
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        }
+
+        let res = await api.post( '/api/blogs' ).send( payload )
+        expect( res.body.likes ).toBe( 0 )
+    } )
+
+    test( 'checking title or url in payload', async () =>
+    {
+        let payload = {
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+        }
+
+        await api.post( "/api/blogs" ).send( payload ).expect( 400 )
+
+        payload = {
+            title: "Type wars",
+            author: "Robert C. Martin",
+            likes: 2,
+        }
+
+        await api.post( "/api/blogs" ).send( payload ).expect( 400 )
+
+    } )
+} );
+
+
+describe( 'DELETE req', () =>
 {
-    let payload = {
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-    }
+    test( 'deleting blogs', async () =>
+    {
+        let allBlogsInDB = await helper.blogInDB()
+        let blogToDelete = allBlogsInDB[ 0 ]
 
-    await api.post( "/api/blogs" ).send( payload ).expect( 201 )
+        await api.delete( `/api/blogs/${ blogToDelete.id }` ).expect( 204 )
 
-    let allBlog = await helper.blogInDB()
-    expect( allBlog.length === helper.initialBlog.length + 1 )
-} )
+        let afterDeletingBlogs = await helper.blogInDB()
+        expect( afterDeletingBlogs.length ).toBe( helper.initialBlog.length - 1 )
 
-test( "likes default 0", async () =>
-{
-    let payload = {
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    }
+        const content = afterDeletingBlogs.map( b => b.author )
+        expect( content ).not.toContain( blogToDelete.author )
+    } )
 
-    let res = await api.post( '/api/blogs' ).send( payload )
-    expect( res.body.likes ).toBe( 0 )
-} )
+    test( 'deleting blogs with invalid ID', async () =>
+    {
+        let invalidID = "5a3d5da59070081a82a3445"
+        await api.delete( `/api/blogs/${ invalidID }` ).expect( 400 )
+    } )
 
-test( 'checking title or url in payload', async () =>
-{
-    let payload = {
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-    }
+    test( 'deleting note with ID which is not in DB', async () =>
+    {
+        let nonExistingId = await helper.nonExistingId()
+        await api.delete( `/api/blogs/${ nonExistingId }` ).expect( 404 )
+    } )
 
-    await api.post( "/api/blogs" ).send( payload ).expect( 400 )
-
-    payload = {
-        title: "Type wars",
-        author: "Robert C. Martin",
-        likes: 2,
-    }
-
-    await api.post( "/api/blogs" ).send( payload ).expect( 400 )
-
-} )
+} );
